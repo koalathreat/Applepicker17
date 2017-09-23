9aaee776be44c55b7f5bc48c5741c6d7831f8a0f
koalathreat <edwin.zheng@rutgers.edu>
2017-09-22 23:13:18 -0400

    Final Draft
    Finished the code for High Score and Score Counter as well as GUI Text.
    Everything should be working.

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+    static public int score = 1000;
+
+    void Awake()
+    {
+       if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePcikerHighScore");
+        }
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GUIText gt = this.GetComponent<GUIText>();
+        gt.text = "High Score: " + score;
+        if(score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+	}
+}
