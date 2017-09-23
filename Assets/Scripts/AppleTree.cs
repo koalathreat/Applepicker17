fc3da17fcc2f45fd87cfb2bd0a58dd324dfdb955
koalathreat <edwin.zheng@rutgers.edu>
2017-09-22 20:23:01 -0400

    Half Done
    Half way finished. Just got to complete code.

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //speed at which the AppleTree moves in meters/second
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the AppleTree wil change directions
+    public float chanceToChangeDirections = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	    //Dropping apples every second	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        
+        //Changing Directions
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //Move right
+        }
+        else if(pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //Move left
+        }
+	}
+
+    void FixedUpdate()
+    {
+        //Chaning Directions Randomly
+        if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //Change Directions
+        }
+    }
+}
